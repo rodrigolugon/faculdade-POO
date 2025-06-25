b5f5bf467d4806474dd5902236c4012c50c1fafb
Rodrigo Alves <rodrigolugonn@gmail.com>
2025-06-25 17:16:51 -0300

    Criando uma classe com tres atributos para representar melhor o triangulo

--- Exemplos/Classe01/Program.cs
+++ Exemplos/Classe01/Program.cs
@@ -6,23 +6,24 @@ namespace Classe01 {
     internal class Program {
         static void Main(string[] args) {
 
-            double xA, xB, xC, yA, yB, yC;
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
 
             Console.WriteLine("Entre com as medida do triangulo X: ");
-            xA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine("Entre com as medida do triangulo Y: ");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p-xA) * (p-xB) * (p-xC));
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p-x.A) * (p-x.B) * (p-x.C));
 
-            p = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(p * (p-yA) * (p-yB) * (p-yC));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p-y.A) * (p-y.B) * (p-y.C));
 
             Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Area de x = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
---
+++ Exemplos/Classe01/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Classe01 {
+    internal class Triangulo {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
