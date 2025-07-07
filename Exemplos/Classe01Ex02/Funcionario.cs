93a558388f6d34a359ec7d4fad85f267a089c673
Rodrigo Alves <rodrigolugonn@gmail.com>
2025-07-07 15:40:34 -0300

    exercicio nome e media de salario

---
+++ Exemplos/Classe01Ex02/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Classe01Ex02 {
+    internal class Funcionario {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Exemplos/Classe01Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Fazer um programa para ler nome e salário de dois funcionários.
+ * Depois, mostrar o salário médio dos funcionários.*/
+using System;
+
+namespace Classe01Ex02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //instanciacao
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            //Em C#, você pode declarar e já atribuir o valor na mesma linha.
+            double media = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine("Media: " + media.ToString("F2"));
+
+        }
+    }
+}
