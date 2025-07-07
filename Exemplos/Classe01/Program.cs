41e719f2d2a8fcd4a7480a8bef276c0cf5d70684
Rodrigo Alves <rodrigolugonn@gmail.com>
2025-07-07 14:58:00 -0300

    adicionando comentarios

--- Exemplos/Classe01/Program.cs
+++ Exemplos/Classe01/Program.cs
@@ -1,15 +1,16 @@
-﻿// Resolvendo um problema sem orientacao a objetos
+﻿//Adicionando uma classe
 using System;
 using System.Globalization;
 
 namespace Classe01 {
     internal class Program {
         static void Main(string[] args) {
-
+            //usamos Nomedaclasse nomestring = new Nomedaclasse();
             Triangulo x = new Triangulo();
             Triangulo y = new Triangulo();
 
             Console.WriteLine("Entre com as medida do triangulo X: ");
+            //Para chamar a variavel usamos nomestring.AtributodaClasse
             x.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
