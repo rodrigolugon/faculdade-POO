a951b487dc63bfc3ab8356b57045a5d60eeb5433
Rodrigo Alves <rodrigolugonn@gmail.com>
2025-07-09 14:30:52 -0300

    Trabalhando com metodo

--- Exemplos/Classe01/Triangulo.cs
+++ Exemplos/Classe01/Triangulo.cs
@@ -1,9 +1,25 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace Classe01 {
     internal class Triangulo {
         public double A;
         public double B;
         public double C;
+
+        //Metodo = funcao dentro da classe
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+
+
+
+
+
+
+
+
     }
 }
